f660343be9dff8a100d45ab65358a219e9ca3e9e
Benny <benedikt@goetemann.de>
2022-03-10 22:42:21 +0100

    added default page and implemented header method for seo stuff

--- pages/index.vue
+++ pages/index.vue
@@ -74,7 +74,35 @@
 
 <script>
 export default {
-  name: "IndexPage",
+  data() {
+    return {
+      title: "Beautiful Landscapes brought to you by the Universe! 🚀",
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Why metaverse when the universe has so much to offer that you haven't seen yet! We bring you to the craziest places in the galaxy!",
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content: "Travel, Flight, Adventure",
+        },
+        {
+          hid: "author",
+          name: "author",
+          content: "Benedikt Götemann",
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- pages/|-default/default.vue
+++ pages/|-default/default.vue
@@ -1,14 +1,4 @@
-<template>
-  <LegalSection>
-    <h1>Datenschutz</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-      eveniet neque omnis veritatis similique, enim, provident ut eligendi
-      itaque quod facere explicabo quae laudantium et soluta dolores rem quo
-      beatae!
-    </p>
-  </LegalSection>
-</template>
+<template></template>
 
 <script>
 export default {
